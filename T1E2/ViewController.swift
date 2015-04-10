10ebc17c5c20b003771c4311691576135fc302c8
edutgn75 <priminet@hotmail.com>
2015-04-10 23:49:55 +0200

    cambio

--- T1E2/ViewController.swift
+++ T1E2/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("Hola amigos")
+        println("Hola mundo")
     }
 
     override func didReceiveMemoryWarning() {
